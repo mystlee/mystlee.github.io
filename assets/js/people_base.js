e2c0ba9a7ed68d9c494d57c9c5045633dd4eb9f0
mystlee <mystlee0801@gmail.com>
2025-05-08 18:04:04 +0900

    update profile and remove dummy data

--- assets/js/people_base.js
+++ assets/js/people_base.js
@@ -2,4 +2,19 @@ window.addEventListener('DOMContentLoaded', () => {
   fetch('/people/geonwoo.html')
     .then(res => res.text())
     .then(data => document.getElementById('geonwoo-placeholder').innerHTML = data);
+  fetch('/people/dongho.html')
+    .then(res => res.text())
+    .then(data => document.getElementById('dongho-placeholder').innerHTML = data);
+  fetch('/people/sarang.html')
+    .then(res => res.text())
+    .then(data => document.getElementById('sarang-placeholder').innerHTML = data);
+  fetch('/people/minsik.html')
+    .then(res => res.text())
+    .then(data => document.getElementById('minsik-placeholder').innerHTML = data);
+  fetch('/people/eunseo.html')
+    .then(res => res.text())
+    .then(data => document.getElementById('eunseo-placeholder').innerHTML = data);
+  fetch('/people/minju.html')
+    .then(res => res.text())
+    .then(data => document.getElementById('minju-placeholder').innerHTML = data);
 });
